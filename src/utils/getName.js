d2e836edd86e23f6759b987a89492eccc70f1a6e
Ydelyn <lorris.colini@orange.com>
2024-02-19 09:48:35 +0100

    1️⃣ first commit

---
+++ src/utils/getName.js
@@ -0,0 +1,13 @@
+import Resume from "../settings/resume.json";
+
+const names = Resume.basics.name.split(" ");
+
+export const FirstName = names[0];
+
+export const Pseudo = names[1];
+
+export const LastName = names[names.length - 1];
+
+export const Initials = FirstName.charAt(0)
+    .toUpperCase()
+    .concat(LastName.charAt(0).toUpperCase());
