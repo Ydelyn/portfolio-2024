75c0e0bcde6839be0f720615a8327d7e4c28bf06
Ydelyn <lorris.colini@orange.com>
2024-02-21 20:33:33 +0100

    🔄 modified profile pic

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,7 @@ import { FirstName, LastName } from "../../utils/getName";
 
 import './About.css';
 
-import profile from '../../assets/profile.png';
+import profile from '../../assets/profile.jpg';
 
 const useStyles = makeStyles((theme) => ({
   main: {
