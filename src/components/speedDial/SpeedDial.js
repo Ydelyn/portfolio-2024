349a2ad31ab0bfb20ef90e845e5852895bdd0abf
Ydelyn <lorris.colini@orange.com>
2024-03-17 21:52:47 +0100

    ✅ added blur on buttons

--- src/components/speedDial/SpeedDial.js
+++ src/components/speedDial/SpeedDial.js
@@ -4,63 +4,64 @@ import { SpeedDial, SpeedDialIcon, SpeedDialAction } from "@material-ui/lab";
 import Resume from "../../settings/resume.json";
 
 const useStyles = makeStyles((theme) => ({
-    speedDial: {
-      position: "fixed",
-      top: theme.spacing(6),
-      right: theme.spacing(6),
-    },
-    icons: {
+  speedDial: {
+    position: "fixed",
+    top: theme.spacing(6),
+    right: theme.spacing(6),
+    '& .MuiSpeedDial-fab': {
       backdropFilter: 'blur(10px)',
     },
-    iconColor: {
-      color: theme.palette.foreground.default,
-      // TODO:
-      // backdropFilter: 'blur(10px)',
-      // borderRadius: "1.25em",
-      // padding: "1.25em",
-    },
+  },
+  blurEffect: {
+  },
+  icons: {
+    backdropFilter: 'blur(10px)',
+  },
+  iconColor: {
+    color: theme.palette.foreground.default,
+  },
 }));
 
 export const SpeedDials = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleOpen = () => {
-      setOpen(true);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    const actionIcons = Resume.basics.profiles.map((action) => (
-      <SpeedDialAction
-        key={action.network.toLowerCase()}
-        icon={<i className={`${action.x_icon} ${classes.iconColor}`}></i>}
-        tooltipTitle={action.network}
-        onClick={handleClose}
-        href={action.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        underline="none"
-        color="inherit"
-        className={classes.icons}
-      />
-    ));
+  const actionIcons = Resume.basics.profiles.map((action) => (
+    <SpeedDialAction
+      key={action.network.toLowerCase()}
+      icon={<i className={`${action.x_icon} ${classes.iconColor}`}></i>}
+      tooltipTitle={action.network}
+      onClick={handleClose}
+      href={action.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      underline="none"
+      color="inherit"
+      className={classes.icons}
+    />
+  ));
 
-    return (
-        <SpeedDial
-          ariaLabel="SpeedDial"
-          className={classes.speedDial}
-          hidden={false}
-          icon={<SpeedDialIcon />}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          open={open}
-          direction="down"
-        >
-          {actionIcons}
-        </SpeedDial>
-    );
+  return (
+    <SpeedDial
+      ariaLabel="SpeedDial"
+      className={`${classes.speedDial} ${open ? '' : classes.blurEffect}`}
+      hidden={false}
+      icon={<SpeedDialIcon />}
+      onClose={handleClose}
+      onOpen={handleOpen}
+      open={open}
+      direction="down"
+    >
+      {actionIcons}
+    </SpeedDial>
+  );
 };
